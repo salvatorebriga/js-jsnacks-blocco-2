905c278a233e547565b6e161b15c9670288ed3fa
salvatorebriga <salvo.z2302@gmail.com>
2024-05-08 14:59:12 +0200

    solution

--- snack-a/main.js
+++ snack-a/main.js
@@ -1,1 +1,46 @@
 "use strict"
+
+const myArray = [];
+
+console.log(myAddElements(myArray));
+
+const n = contaElementi(myArray);
+
+console.log('Elementi presenti: ' + n);
+
+console.log(myArray);
+
+function myAddElements(functionMyArray){
+
+    let i = 0;
+    let bool =  false;
+
+    do{
+        const temp = prompt('Inserisci qualcosa, oppure digita "esci" per uscire');
+
+        if(temp !== 'esci'){
+            functionMyArray[i] = temp;
+            i++;
+        }else{
+            bool = true;
+        }
+    }while(bool == false);
+
+    return 'fine inserimento';
+}
+
+function contaElementi(functionMyArray){
+
+    let i = 0;
+    let bool =  false;
+
+    while(bool == false){
+        if(functionMyArray[i] == undefined){
+            bool = true;
+        }else{
+            i++;
+        }
+    }
+
+    return i;
+}
